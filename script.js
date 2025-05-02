732286775eb7ec3581e20c297f1bfed01696cd19
ayuknkem joshua <joesolo165@gmail.com>
2025-05-02 11:15:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  result = prompt("Hello what is your name?");
+  confirm(`verify if it is indeed ${result}`);
+  let isname = confirm("have you verified?");
+  alert(`ok ${result}; welcome to guitar accedemy`);
+
+  
